05f26e7f87c99b7f2c7bb07468dac5d0fd37cd9f
Luis Carlos Enrique Herrera Santos <leherreras@gmail.com>
2019-11-08 20:35:10 -0500

    endpoints to show notes for specific user

--- src/apps/diary/controller.py
+++ src/apps/diary/controller.py
@@ -1,2 +1,6 @@
 def create_note(data):
     pass  # TODO create note in DB
+
+
+def get_notes(query=None):
+    pass  # TODO Get notes from DB
--- src/apps/diary/view.py
+++ src/apps/diary/view.py
@@ -1,8 +1,9 @@
 import flask_login
 from flask import render_template, request, url_for, flash, redirect
 from flask_classy import FlaskView
+from flask_paginate import get_page_parameter, Pagination
 
-from .controller import create_note
+from .controller import create_note, get_notes
 
 
 class LoginView(FlaskView):
@@ -16,3 +17,17 @@ class LoginView(FlaskView):
         message = create_note(data)
         flash(message)
         return redirect(url_for('LoginView:index'))
+
+    def notes(self):
+        """
+        use this library https://flask-paginate.readthedocs.io/en/latest/
+        :return:
+        """
+        search = False
+        q = request.args.get('q')
+        if q:
+            search = True
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        notes = get_notes(query=q)
+        pagination = Pagination(page=page, total=notes.count(), search=search, record_name='notes')
+        return render_template('notes.html', notes=notes, pagination=pagination)
