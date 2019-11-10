a2803e03c5c95581b22647cd48ba2e83e44b0582
Jenny Ossa <jeossadu@poligran.edu.co>
2019-11-10 17:50:14 -0500

    ramaBDprueba

--- src/apps/authentication/controller.py
+++ src/apps/authentication/controller.py
@@ -1,4 +1,4 @@
 
 
 def authenticate(email, password):
-    return True  # TODO Return the user object with is_authenticated, is_active, is_anonymous, get_id()
+    return True  # TODO Return the user object with is_authenticated, is_active, is_anonymous, get_id().
