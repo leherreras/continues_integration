3f544fc53abab6a14272c6a9621966243c061605
Jenny Ossa <jeossadu@poligran.edu.co>
2019-11-10 17:40:22 -0500

    Mod controller.py email-pwd

--- src/apps/authentication/controller.py
+++ src/apps/authentication/controller.py
@@ -1,4 +1,4 @@
 
 
-def authenticate(email: str, password: str):
+def authenticate(email, password):
     return True  # TODO Return the user object with is_authenticated, is_active, is_anonymous, get_id()
