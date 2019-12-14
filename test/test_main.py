5cb27abd8d31b6d59f9a5bea295e249739903f16
lherrera <luis.herrera@cyxtera.com>
2019-12-14 09:54:16 -0500

    create unittest

--- test/test_main.py
+++ test/test_main.py
@@ -2,8 +2,17 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_first(self):
+        self.assertEqual(True, True)
+
+    def test_2(self):
+        self.assertEqual(True, True)
+
+    def test_3(self):
+        self.assertEqual(True, True)
+
+    def test_4(self):
+        self.assertEqual(True, True)
 
 
 if __name__ == '__main__':
